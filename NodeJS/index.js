4132b1108533a3b60e73c56085065f44b88462e4
Jonas Mumm <jonasmumm@web.de>
2025-08-09 18:16:08 +0200

    fake data poll

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,26 +1,65 @@
 const express = require('express');
+const { version } = require('os');
 const app = express();
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const dbPath = path.resolve(__dirname, "data/constants.db");
 const conDb = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, err => {
-    if(err) console.error("Error while opeining database: "+err)
+    if (err) console.error("Error while opeining database: " + err)
 });
 
-var cardConstants = conDb.all("SELECT * FROM cards INNER JOIN cardEffects on cards.name = cardEffects.cardname GROUP BY cardEffects.cardname",(err,rows)=>{
-    if(err) console.error("Error while loading cards: "+err);
+var conGeneral = {
+    numTurns: 12,
+    startEnergy: 10,
+    numCardsOnHand: 4,
+    maxPlayers: 1
+}
 
-    console.log(JSON.stringify(rows))
+var constants = {};
+
+conDb.all("SELECT * FROM cards", (err, rows) => {
+
+    var allCards = rows;
+    conDb.all("SELECT * FROM cardEffects", (err, rows) => {
+
+        var allCardEffects = rows;
+
+        allCards.forEach(card => {
+            card.effects = allCardEffects.filter((e) => e.cardName == card.name);
+        });
+
+        constants.cards = allCards;
+    });
 });
 
-var constants = null;
+const sessions = [];
 
-app.get("/poll",(req, res) => {
-    res.send("Hello it me "+req.query.user);
+app.get("/poll", (req, res) => {
+    var user = req.query.user
+    //var session = GetOrCreateSession();
+
+    var dummyState = {
+        turnIndex: 0,
+        phaseIndex: 0,
+        players: [
+            GenerateRandomPlayer(0, 1 + 0 * conGeneral.numCardsOnHand),
+            GenerateRandomPlayer(1, 1 + 1 * conGeneral.numCardsOnHand),
+            GenerateRandomPlayer(2, 1 + 2 * conGeneral.numCardsOnHand),
+            GenerateRandomPlayer(3, 1 + 3 * conGeneral.numCardsOnHand),
+        ],
+        completedGroupTasks : 0,
+        ended : false,
+        version : 1,
+        cardStack: [
+            GenerateRandomCard(1 + 4* conGeneral.numCardsOnHand)
+        ]
+    }
+
+    res.send(JSON.stringify(dummyState));
 });
 
 app.use((req, res) => {
-    console.log("Receive unhandled"+req.method+" at url:"+ JSON.stringify(req.url)+" query:"+JSON.stringify(req.query))
+    console.log("Receive unhandled" + req.method + " at url:" + JSON.stringify(req.url) + " query:" + JSON.stringify(req.query))
 });
 
 var port = process.env.port || 8081;
@@ -29,3 +68,28 @@ app.listen(port, () => {
     console.log(`App listening on port ${port}`);
     console.log('Press Ctrl+C to quit.');
 });
+
+function GenerateRandomCard(cardId) {
+    var cardInstance = structuredClone(constants.cards[0]);
+    cardInstance.id = cardId
+    return cardInstance;
+}
+
+function GenerateRandomPlayer(number, cardId) {
+    var playerCards = [];
+    for (let index = 0; index < conGeneral.numCardsOnHand; index++) {
+        playerCards.push(GenerateRandomCard(cardId));
+        cardId+=1
+    }
+
+    return {
+        name: "Player" + number,
+        energy: conGeneral.startEnergy,
+        hand: playerCards,
+        lastDiceResult: 0,
+        lastCardIdSelected: null,
+        completedPersonalTasks: 0,
+        stagedCard : null,
+        joinDate : Date.now()
+    }
+}
